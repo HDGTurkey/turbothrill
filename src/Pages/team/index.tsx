3aa75b7c622d03c5ade01424f290c73eee7b9a78
mucahidkincir <mucahitkincir@hotmail.com>
2023-06-01 19:30:17 +0300

    Add clouddb

--- src/Pages/team/index.tsx
+++ src/Pages/team/index.tsx
@@ -1,17 +1,31 @@
-import React, { Children } from 'react'
-import { useSite } from '../../app/Context/Context'
+import React, { Children, useContext, useEffect, useState } from 'react'
 import TeamCard from '../../app/Components/Cards/TeamCard';
 import PageTitle from '../../app/Components/PageTitle';
-import TeamData from '../../app/data/team/team.json';
+//import TeamData from '../../app/data/team/team.json';
+import { AGCContext } from '@/app/Context/AGCProvider';
+import { teams } from "../../app/model/teams";
 
 
 export default function Team() {
+  const agcContext = useContext(AGCContext)
+  const [teamData, setTeamData] = useState([]);
+  const [loading, setLoading] = useState(false)
+
+  async function getTeams() {
+    setLoading(true);
+    setTeamData(await agcContext.executeQuery(teams));
+    setLoading(false);
+  }
+  useEffect(()=>{
+    getTeams();
+  },[]);
   return (
     <>
       <div className='mx-8'>
         <PageTitle title='Team' />
+        {loading ? "Loading" : 
         <div className='max-w-7xl mx-auto p-3 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 pb-56  '>
-          {TeamData && TeamData.map((single, key) => {
+          {teamData && teamData.map((single, key) => {
             return (
               <div key={key} className="col mb-6" data-aos="fade-up" data-aos-delay="300">
                 <TeamCard teamData={single} key={key} ></TeamCard>
@@ -20,6 +34,7 @@ export default function Team() {
           })}
           {/* title='Ambassador' name='Elif Emre' photo='https://media-exp1.licdn.com/dms/image/C4D03AQEdC-81qGu3-Q/profile-displayphoto-shrink_800_800/0/1662140248272?e=1676505600&v=beta&t=qCkJuk8H7KMMrrLbBoejg3KVaCRUk964e0ruQjNdero' linkedin='https://www.linkedin.com/in/elifemr/' github='https://github.com/elselif' twitter='https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoidHIifQ%3D%3D%22%7D' */}
         </div>
+        }
       </div>
     </>
   )
---
+++ src/app/Context/AGCProvider.tsx
@@ -0,0 +1,55 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  AGConnectCloudDB,
+  CloudDBZoneQuery,
+  CloudDBZoneConfig,
+  CloudDBZone,
+} from "@hw-agconnect/database";
+import agconnect from "@hw-agconnect/api";
+import "@hw-agconnect/auth";
+import "@hw-agconnect/instance";
+import { context } from "../../app/config/agconnect-services.js";
+
+export const AGCContext = createContext<any>({executeQuery: undefined});
+type x = {
+  children: JSX.Element;
+};
+export const AGCProvider: React.FC<x> = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [cloudDbZone, setCloudDbZone] = useState<CloudDBZone | undefined>(
+    undefined
+  );
+
+  async function init() {
+    setLoading(true);
+
+    agconnect.instance().configInstance(context);
+    AGConnectCloudDB.initialize(context);
+    const schema = require("../../app/config/Data.json");
+    const agcCloudDB = AGConnectCloudDB.getInstance();
+    agcCloudDB.createObjectType(schema);
+    const config = new CloudDBZoneConfig("cloudzone");
+    const a = await agcCloudDB.openCloudDBZone(config);
+    setCloudDbZone(a);
+    
+    setLoading(false);
+  }
+
+  //@ts-ignore
+  async function executeQuery(query) {
+        const zoneQuery = CloudDBZoneQuery.where(query);
+        //@ts-ignore
+        const snapshot = await cloudDbZone.executeQuery(zoneQuery);
+        return snapshot.getSnapshotObjects();
+  }
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
+  return <AGCContext.Provider value={{ executeQuery }}>{children}</AGCContext.Provider>;
+};
