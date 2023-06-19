962e6bbe754195a4648ec35b3441a0dcfd47bd9d
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2023-06-19 23:25:55 +0300

    fixed

--- src/Pages/_app.js
+++ src/Pages/_app.js
@@ -1,13 +1,14 @@
-<<<<<<< Updated upstream
-import Footer from "../app/partials/Footer";
-import Navbar from "../app/partials/Navbar";
-import SiteProvider, { useSite } from "../app/Context/Context";
-import "../app/globals.css";
+import Footer from '../app/partials/Footer'
+import Navbar from '../app/partials/Navbar'
+import SiteProvider, { useSite } from '../app/Context/Context'
 
-import { AGCProvider } from "@/app/Context/AGCProvider";
+// import '../styles/index.scss'
+import '../app/globals.css'
+
+import { AGCProvider } from '@/app/Context/AGCProvider'
 
 export default function MyApp({ Component, pageProps }) {
-  const { theme } = useSite();
+  const { theme } = useSite()
   return (
     <AGCProvider>
       <SiteProvider>
@@ -16,23 +17,5 @@ export default function MyApp({ Component, pageProps }) {
         <Footer />
       </SiteProvider>
     </AGCProvider>
-  );
-=======
-import Footer from '../app/partials/Footer'
-import Navbar from '../app/partials/Navbar'
-import SiteProvider, { useSite } from '../app/Context/Context'
-
-// import '../styles/index.scss'
-import '../app/globals.css'
-export default function MyApp({ Component, pageProps }) {
-  const { theme } = useSite()
-
-  return (
-    <SiteProvider>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </SiteProvider>
   )
->>>>>>> Stashed changes
 }
