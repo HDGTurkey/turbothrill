d2b01012a488df468bb635d97bee412945f6a3e4
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2023-07-26 12:57:47 +0300

    reset

--- src/app/globalState/states.tsx
+++
@@ -1,7 +0,0 @@
-import { createGlobalState } from 'react-hooks-global-state'
-
-const { setGlobalState, useGlobalState } = createGlobalState({
-	isPagginated: 'false',
-})
-
-export { setGlobalState, useGlobalState }
