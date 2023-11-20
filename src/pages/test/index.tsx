7d3e316092dcef3ac401e9fd46be9f4dfd8b41fb
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2023-11-20 19:27:41 +0300

    return added committe

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -5,24 +5,22 @@ const accordion_items = [
   {
     id: "one",
     show: true,
-    title: "Do you enjoy using social media platforms effectively?",
+    title:
+      "Do you enjoy using social media platforms effectively?",
     desc: "Yes, I am interested in content sharing and interaction.",
     descNo: "No, social media platforms are not that important to me.",
   },
   {
     id: "two",
     show: true,
-    title:
-      "Would you like to increase interaction and organize events among the members of the community?",
+    title: "Would you like to increase interaction and organize events among the members of the community?",
     desc: "Yes, I like to get people together and plan events.",
-    descNo:
-      "No, it's not ideal for me to organize events and manage community interactions.",
+    descNo: "No, it's not ideal for me to organize events and manage community interactions.",
   },
   {
     id: "three",
     show: true,
-    title:
-      "Do you enjoy organizing cultural events , excursions or entertainment organizations ?",
+    title: "Do you enjoy organizing cultural events , excursions or entertainment organizations ?",
     desc: "Yes, I like to have fun by bringing people together with different activities.",
     descNo: "No, I'm not interested.",
   },
@@ -50,11 +48,9 @@ const accordion_items = [
   {
     id: "seven",
     show: true,
-    title:
-      "Do you like to moderate on internet platforms or chat applications?",
+    title: "Do you like to moderate on internet platforms or chat applications?",
     desc: "Yes, it's OK for me to edit and moderate community interaction.",
-    descNo:
-      "No, it's not appropriate for me to edit and moderate community interaction.",
+    descNo: "No, it's not appropriate for me to edit and moderate community interaction.",
   },
   {
     id: "eight",
@@ -77,16 +73,15 @@ const questions = [
 ];
 
 const news_letter_contents = {
-  bg_img: "",
-  subtitle: "Get update",
-  title: "Get latest updates and deals",
-  btn_text: "Subscribe",
-  news_l_img: "/assets/img/news/news-1.png",
-  shape_img: "",
-};
+  bg_img: '',
+  subtitle: 'Get update',
+  title: 'Get latest updates and deals',
+  btn_text: 'Subscribe',
+  news_l_img: '/assets/img/news/news-1.png',
+  shape_img: ''
+}
 
-const { bg_img, btn_text, news_l_img, shape_img, subtitle, title } =
-  news_letter_contents;
+const { bg_img, btn_text, news_l_img, shape_img, subtitle, title } = news_letter_contents;
 
 const Test: React.FC = () => {
   const [answers, setAnswers] = useState<Array<string | null>>(
@@ -116,7 +111,8 @@ const Test: React.FC = () => {
       return "Social Media Committee or Engagement Committee may be a good fit for you 😎";
     } else if (totalYesCount === accordion_items.length / 2) {
       return "Social Life Committee, Technical Activity Committee or Content/Editorial Committee may be considered for you 😎";
-    } else {
+    }
+    else {
       return `The Career Development Committee, Discord Committee, or Social Responsibility Committee may be more suited to your abilities 😎`;
     }
   };
@@ -158,71 +154,21 @@ const Test: React.FC = () => {
           {accordion_items.map((item, index) => {
             const { id, show, title, desc, descNo } = item;
             return (
-              <div key={index} className="card w-full rounded-md p-6 shadow">
-                <p className=" flex text-xl text-black">
-                  {index + 1} ) {title}
-                </p>
-                <div className="grid gap-2 p-3 sm:grid-cols-2">
-                  <label className="dark:bg-slate-900 flex w-full rounded-md  border-gray-200 bg-white p-3 text-sm focus-within:border-blue-500 focus-within:ring-blue-500 dark:border-gray-700 dark:text-gray-400">
-                    <input
-                      type="radio"
-                      name={`question-${index}`}
-                      value="yes"
-                      checked={answers[index] === "yes"}
-                      onChange={() => handleAnswerChange(index, "yes")}
-                      className="hidden"
-                    />
-                    <div className="relative h-6 w-6 scale-100 transform rounded-full border border-blue-600 bg-white transition-transform duration-300 dark:border-gray-700 dark:bg-gray-800 dark:ring-offset-gray-800">
-                      {answers[index] === "yes" && (
-                        <div className="absolute inset-0 flex items-center justify-center rounded-full bg-blue-500 dark:bg-blue-500">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-4 w-4 text-white"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M6.293 9.293a1 1 0 011.414 0L9 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-2-2a1 1 0 010-1.414z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </div>
-                      )}
-                    </div>
-                    <span className="ml-3 text-sm text-gray-700">{desc}</span>
+              <div key={index} className="card w-full rounded-md shadow- my-16 p-6">
+                <p className="text-black text-xl">{title}</p>
+                <div className="grid sm:grid-cols-2 gap-2 p-5">
+                  <label className="flex p-3 block w-[%50] bg-white border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
+                    <input type="radio" name={`question-${index}`} value="yes" checked={answers[index] === "yes"} onChange={() => handleAnswerChange(index, "yes")} className="shrink-0 mt-0.5 border-gray-200 rounded-full text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-radio-in-form" />
+                    <span className="text-sm text-gray-700 ml-3 ">{desc}</span>
                   </label>
 
-                  <label className="dark:bg-slate-900 flex w-full rounded-md  border-gray-200 bg-white p-3 text-sm focus-within:border-blue-500 focus-within:ring-blue-500 dark:border-gray-700 dark:text-gray-400">
-                    <input
-                      type="radio"
-                      name={`question-${index}`}
-                      value="no"
-                      checked={answers[index] === "no"}
-                      onChange={() => handleAnswerChange(index, "no")}
-                      className="hidden"
-                    />
-                    <div className="relative h-6 w-6 scale-100 transform rounded-full border border-gray-200 bg-white transition-transform duration-300 dark:border-gray-700 dark:bg-gray-800 dark:ring-offset-gray-800">
-                      {answers[index] === "no" && (
-                        <div className="absolute inset-0 flex items-center justify-center rounded-full bg-blue-500 dark:bg-blue-500">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-4 w-4 text-white"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M6.293 9.293a1 1 0 011.414 0L9 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-2-2a1 1 0 010-1.414z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </div>
-                      )}
-                    </div>
-                    <span className="ml-3 text-sm text-gray-700">{descNo}</span>
+                  <label className="flex p-3 block w-full bg-white border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
+                    <input type="radio" name={`question-${index}`} value="no" checked={answers[index] === "no"} onChange={() => handleAnswerChange(index, "no")} className="shrink-0 mt-0.5 border-gray-200 rounded-full text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-radio-checked-in-form" />
+                    <span className="text-sm text-gray-700 ml-3 ">{descNo}</span>
                   </label>
                 </div>
+
+
               </div>
             );
           })}
@@ -242,38 +188,27 @@ const Test: React.FC = () => {
             // >
             //   {getResultMessage()}
             // </div>
-            <div
-              className="tp-news-letter-area pt-140 wow tpfadeUp"
-              data-wow-duration=".7s"
-              data-wow-delay=".4s"
-            >
+            <div className="tp-news-letter-area pt-140 wow tpfadeUp" data-wow-duration=".7s" data-wow-delay=".4s">
               <div className="container">
-                <div
-                  className="tp-news-letter-box p-relative"
-                  style={{
-                    backgroundImage: `url(${bg_img})`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "right",
-                    backgroundSize: "contain",
-                  }}
-                >
+                <div className="tp-news-letter-box p-relative" style={{ backgroundImage: `url(${bg_img})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'right', backgroundSize: 'contain' }} >
                   <div className="row">
                     <div className="  ">
                       <div className="tp-news-wrapper  z-index-1">
                         <div className="tp-news-letter-section-box">
-                          <h2 className="tp-title text-black">
-                            {getResultMessage()}
-                          </h2>
+                          <h2 className="tp-title text-black">{getResultMessage()}</h2>
                         </div>
                         <div className="post__button">
-                          <a className="tp-btn-border text-black">
+                          <a className="tp-btn-border text-black" >
                             {" "}
                             Join the Committee
                           </a>
                         </div>
+
                       </div>
                     </div>
+
                   </div>
+
                 </div>
               </div>
             </div>
